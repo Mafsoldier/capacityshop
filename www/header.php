f865ff11b49312d655eff6578872943358fc6306
Marc Willemsen <marc@bolt.nl>
2019-11-16 21:32:57 +0100

    v 0.0.1

---
+++ www/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Capacity shop home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+
+
+
+</head>
+
+
+<body>
+
+    <div id="header"> 
+
+        <div class="container">
+        <a href="index.php"> Capacity shop </a>
+            <ul>
+                <li>
+                 <a href="index.php">Home </a>
+                </li>
+                <li> 
+                <a href="contact.php"> Contact </a>
+                </li>
+                <li> 
+                <a href="blog.php"> Blog </a>
+                </li>
+                <li> 
+                <a href="portfolio.php"> Portfolio </a>
+                </li>
+            
+            </ul>
+        </div>
+
+
+     </div>
--- www/index.php
+++ www/index.php
@@ -1,16 +1,21 @@
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Capacity shop home</title>
-    
+
+<?php 
+include_once("header.php");
+?>
+
+
+<div id="content"> content </div>
 
 
 
-</head>
-<body>
-    
-</body>
-</html>
+
+
+
+
+
+
+
+<?php 
+include_once("footer.php");
+?>
+
