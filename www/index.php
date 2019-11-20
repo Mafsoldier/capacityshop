1a4677672fdd0bc67b0552e9b798969f69267eee
Marc Willemsen <marc@bolt.nl>
2019-11-20 20:36:27 +0100

    v 0.0.1

--- www/index.php
+++ www/index.php
@@ -1,21 +1,35 @@
-
-<?php 
-include_once("header.php");
-?>
-
-
-<div id="content"> content </div>
-
-
-
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Capacity shop</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 
 
 
 
+<div class="container">
 
-<?php 
-include_once("footer.php");
-?>
 
+<div class="item-header"></div>
+<div class="item-main"></div>
+
+<div class="item-sidebar"></div>
+
+<div class="item-footer"></div>
+
+
+</div>
+
+
+
+
+
+
+   
+</body>
+</html>
